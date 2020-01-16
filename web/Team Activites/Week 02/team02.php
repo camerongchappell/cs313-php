b27807c916d5d0044d6418deaf157c354900ddf9
Cameron Chappell <cameron.g.chappell@gmail.com>
2020-01-16 16:17:40 -0700

    Updating

--- web/Team Activites/Week 02/team02.php
+++ web/Team Activites/Week 02/team02.php
@@ -1,10 +1,6 @@
 <?php
 for ($i = 0; $i <= 10; $i++) { ?>
-    <div style="width: 50%; float:left"<?php echo $i; ?>" class="numberedDivs"
-        <?php if($i % 2 == 0){echo "style='color:red'";}?>>
-    	This is a div #<?php echo $i; ?></div>
-for ($i = 0; $i <= 10; $i++) { ?>
-    <div style="width: 50%; float:right"<?php echo $i; ?>" class="numberedDivs"
+    <div style="width: 50%; float:left" id="<?php echo $i; ?>" class="numberedDivs"
         <?php if($i % 2 == 0){echo "style='color:red'";}?>>
     	This is a div #<?php echo $i; ?></div>
 <?php }?>
