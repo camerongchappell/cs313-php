179e0c00cc107fa6f8981d720d1c1fb0b705e5c1
Cameron Chappell <cameron.g.chappell@gmail.com>
2020-01-18 22:11:15 -0700

    Adding assignment02

---
+++ web/Assignments/assign02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Cameron Chappell's Home Page</title>
+<link rel="stylesheet" type="text/css" href="assign02.css">
+<script src="assign02.js"></script>
+</head>
+
+<body>
+
+<h1>Welcome to Cameron Chappell's Home Page</h1>
+<hr>
+<p class="sentence">My name is Cameron Chappell, my major is computer science,and I am from Vicksburg, Mississippi.  I like to hang out<br>with family and friends, go snowboarding, play paintball and video games, and watch the tv show <em>The Office</em>.  Which is<br>where my quote is from.</p>
+
+<p class="sentence">Here is one of my favorite quotes:</p><blockquote class="sentence" cite="https://uproxx.com/tv/quotes-from-the-office-michael-scotts-best-lines/">"You miss 100% of the shots you don't take. -Wayne Gretzky"-Michael Scott</blockquote>
+<img src="me.jpg" alt = "Picture of me">
+<button type="button" onclick="buttonClick();">Click Me</button><br><br>
+<a href="assignments.html">CS 313 Assignments</a>
+
+</body>
+
+</html>
