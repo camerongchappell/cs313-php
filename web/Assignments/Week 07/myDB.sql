c527b763a16134e9f8282b26399926fb209b0841
camerongchappell <40769646+camerongchappell@users.noreply.github.com>
2020-02-23 00:22:07 -0700

    Update myDB.sql

--- web/Assignments/Week 07/myDB.sql
+++ web/Assignments/Week 07/myDB.sql
@@ -16,28 +16,11 @@ CREATE TABLE public.gadget (
    suit_id        INT            NOT NULL    REFERENCES        public.suit(id)
 );
 
-INSERT INTO public.user (username, password, display_name)
-VALUES ('camchap1', 'student', 'Cameron Chappell');
+ALTER TABLE suit
+ADD COLUMN image VARCHAR(500)
 
-INSERT INTO public.suit (name, color)
-VALUES ('Advanced Suit', 'Red, Blue, White');
-
-INSERT INTO public.gadget (name, suit_id)
-VALUES ('Web Shooters', '1');
-
-UPDATE suit
-SET year_created = '2018'
-WHERE id = '1';
-
-DELETE FROM suit
-WHERE name = 'Advanced Suit';
+ALTER TABLE suit
+ADD COLUMN year_created VARCHAR(100)
 
 ALTER TABLE suit
 ADD COLUMN info VARCHAR(255)
-
-ALTER TABLE gadget
-DROP CONSTRAINT gadget_name_key;
-
-UPDATE suit
-SET info = E'Created for the PlayStation 4 exclusive, Marvel\'s Spider-Man.'
-WHERE id = '1';
